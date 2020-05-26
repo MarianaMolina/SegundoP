ebd039edfb021e9136c505f62c940c24d477bcc8
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2020-05-25 19:15:15 -0600

    DAO y DAOlmpl

--- SegundoParcial/src/main/java/com/parcial/demo/dao/LibroDAO.java
+++ SegundoParcial/src/main/java/com/parcial/demo/dao/LibroDAO.java
@@ -1,5 +1,19 @@
 package com.parcial.demo.dao;
 
-public interface LibroDAO {
+import java.util.List;
 
+import org.springframework.dao.DataAccessException;
+
+import com.parcial.demo.domain.Categoria;
+import com.parcial.demo.domain.Libro;
+
+
+public interface LibroDAO {
+	
+	public List<Libro> findBook() throws DataAccessException;
+	
+	public void saveBook(Libro libro) throws DataAccessException;
+	
+	public void saveCategory(Categoria categoria) throws DataAccessException;
+	
 }
--- SegundoParcial/src/main/java/com/parcial/demo/dao/LibroDAOlmpl.java
+++ SegundoParcial/src/main/java/com/parcial/demo/dao/LibroDAOlmpl.java
@@ -1,5 +1,65 @@
 package com.parcial.demo.dao;
 
-public class LibroDAOlmpl {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import com.parcial.demo.domain.Categoria;
+import com.parcial.demo.domain.Libro;
+
+@Repository
+public class LibroDAOlmpl implements LibroDAO{
+	
+	@PersistenceContext(unitName="demo")
+	private EntityManager entityManager;
+
+	@Override
+	public List<Libro> findBook() throws DataAccessException {
+		StringBuffer sb = new StringBuffer();
+		sb.append("select * from public.cat_libro");
+		
+		Query query = entityManager.createNativeQuery(sb.toString(),Libro.class);
+		List<Libro> result = query.getResultList();
+		return result;
+	}
+
+	@Override
+	public void saveBook(Libro libro) throws DataAccessException {
+		try {
+			if(libro.getId_libro() == null) {
+				entityManager.persist(libro);
+			}
+			else {
+				entityManager.merge(libro);
+				entityManager.flush();
+			}
+		}catch(Throwable e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void saveCategory(Categoria categoria) throws DataAccessException {
+		try {
+			if(categoria.getId_categoria() == null) {
+				entityManager.persist(categoria);
+			}
+			else {
+				entityManager.merge(categoria);
+				entityManager.flush();
+			}
+		}catch(Throwable e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
 }
