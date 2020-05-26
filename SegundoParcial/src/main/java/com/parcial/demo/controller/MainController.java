fdf17e7b8657a7163e9b9dca37c0f32e1455f403
Mariana Molina <37163483+MarianaMolina@users.noreply.github.com>
2020-05-25 19:39:40 -0600

    estructura html

--- SegundoParcial/src/main/java/com/parcial/demo/controller/MainController.java
+++ SegundoParcial/src/main/java/com/parcial/demo/controller/MainController.java
@@ -1,5 +1,27 @@
 package com.parcial.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.parcial.demo.service.LibroService;
+
+
+@Controller
 public class MainController {
+	
+	@Autowired
+	private LibroService libroService;
+	
+	//inicio
+	@RequestMapping ("/index")
+	public ModelAndView Main() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("main");
+		return mav;
+	}
+	
+
 
 }
